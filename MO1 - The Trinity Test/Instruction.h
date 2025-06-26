6eea945af4e85b58e356b9cf1b5dcd2fc325e8c5
Michael Gabriel A. Del Rosario <92383206+Subprograms@users.noreply.github.com>
2025-06-26 12:24:28 +0800

    Update Instruction.h

--- MO1 - The Trinity Test/Instruction.h
+++ MO1 - The Trinity Test/Instruction.h
@@ -2,33 +2,23 @@
 #define INSTRUCTION_H
 
 #include <string>
+#include <vector>
 
-enum class OpCode {
-    PRINT,
-    DECLARE,
-    ADD,
-    SUBTRACT,
-    SLEEP,
-    FOR_BEGIN,
-    FOR_END
-};
+enum class OpCode {PRINT, DECLARE, ADD, SUBTRACT, SLEEP, FOR, NOP};
 
 struct Instruction {
     OpCode op;
-    std::string arg1;
-    std::string arg2;
-    std::string arg3;
-    bool isArg2Var = true;
-    bool isArg3Var = true;
+    std::vector<Instruction> body;
+    std::string arg1, arg2, arg3;
+    bool isArg2Var{false}, isArg3Var{false};
 
-    Instruction(OpCode opCode = OpCode::PRINT,
-                const std::string& a1 = "",
-                const std::string& a2 = "",
-                const std::string& a3 = "",
-                bool arg2Var = true,
-                bool arg3Var = true)
-        : op(opCode), arg1(a1), arg2(a2), arg3(a3),
-          isArg2Var(arg2Var), isArg3Var(arg3Var) {}
+    Instruction(OpCode o = OpCode::NOP) : op(o) {}
+    Instruction(OpCode o, std::vector<Instruction> b,
+                std::string a1, std::string a2)
+        : op(o), body(std::move(b)), arg1(std::move(a1)), arg2(std::move(a2)) {}
+    Instruction(OpCode o, std::string a1, std::string a2,
+                std::string a3 = "", bool v2 = false, bool v3 = false)
+        : op(o), arg1(std::move(a1)), arg2(std::move(a2)), arg3(std::move(a3)),
+          isArg2Var(v2), isArg3Var(v3) {}
 };
-
 #endif
